0479cb2e04d08633c09698e28e5f206aeea94366
Magnus-Harder <magnusharder@gmail.com>
2023-01-21 15:12:55 +0100

    Adding Masks)

--- Transformer.py
+++ Transformer.py
@@ -6,29 +6,55 @@ from torch.nn import functional as F
 
 
 
-# Embed(Vocabsize, embeddim) - 
-# 
+from torch import nn
 
-
-class Transformer(nn.Module):
-    def __init__(self):
+#%%
+class Embedder(nn.Module):
+    def __init__(self, vocab_size, d_model):
         super().__init__()
-        self.Transformer = nn.Transformer(d_model=300,nhead=10,num_encoder_layers=6,num_decoder_layers=6,dim_feedforward=2048,dropout=0.1,activation='relu')
-        self.Linear = nn.Linear(25*300,204)
-        self.Softmax = nn.Softmax(dim=204)
+        self.embed = nn.Embedding(vocab_size, d_model)
+    def forward(self, x):
+        return self.embed(x)
+
+#%%
+
+# Define function to create src_mask
+def src_mask(D,n_pads):
+    mask = tn.full((D,D),float(-1e9))
+    mask[:D-n_pads,:D-n_pads] = 0
+    return mask
+
+# Define function to create tgt_mask
+def tgt_mask(D,n_pads):
+    mask = tn.full((D,D),float('-inf'))
+    mask = mask.triu(diagonal=1)
+    return mask
+
+# Initiate Transformer model
+class Transformer(nn.Module):
+    def __init__(self,src_vocab_size,tgt_vocab_size,d_model,nhead):
+        super().__init__()
+        self.Transformer = nn.Transformer(d_model=d_model,nhead=nhead,num_encoder_layers=2,num_decoder_layers=2,dim_feedforward=1024,dropout=0.1,activation='relu',batch_first=True)
+        self.Linear = nn.Linear(d_model,tgt_vocab_size)
 
 
-    def forward(self, X):
-        X = self.Transformer(X)
-        X.reshape(-1,25*300)
-        X = self.Linear(X)
-        X = self.Softmax(X)
+        self.src_embed = Embedder(src_vocab_size, d_model)
+        self.tgt_embed = Embedder(tgt_vocab_size, d_model)
+        self.Sotftmax  = nn.Softmax(dim=1)
 
 
 
-
-        return X
-
+    def forward(self, X,tgt,src_mask=None,tgt_mask=None,src_key_padding_mask=None,tgt_key_padding_mask=None,memory_key_padding_mask=None):
+        X = self.src_embed(X)
+        tgt = self.tgt_embed(tgt)
+        
+        out = self.Transformer(X,tgt,src_mask = src_mask ,
+                                tgt_mask = tgt_mask,
+                                src_key_padding_mask = src_key_padding_mask,
+                                tgt_key_padding_mask = tgt_key_padding_mask)
+        out = self.Linear(out)
+        out = self.Sotftmax(out)
+        return out
 
 
 # %%
