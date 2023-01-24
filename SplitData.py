f470f1768db5408a26a8e3e3d4ac3f2cb3fc05b6
Magnus-Harder <magnusharder@gmail.com>
2023-01-24 11:45:23 +0100

    Fix SplitData File

--- SplitData.py
+++ SplitData.py
@@ -1,10 +1,9 @@
 #%%
-import torch as tn
+import torch as th
 from torchtext import vocab
 import pickle as pl
-from Transformer_Builtin import Transformer,src_mask,tgt_mask
-from tqdm import tqdm
-import numpy as np
+
+
 
 # Load English data and French data
 with open('data/English_encodings.pkl', 'rb') as f:
@@ -15,13 +14,14 @@ with open('data/French_encodings.pkl', 'rb') as f:
 # Get the vocabulary size
 src_vocab_size = Vocab_fr.__len__()
 tgt_vocab_size = Vocab_en.__len__()
-#%%
 
-X = tn.tensor(french_encodings)
-Y = tn.tensor(english_encodings)
+d_model = 512
+# Define Train/test split and Masking
+X = th.tensor(french_encodings)
+Y = th.tensor(english_encodings)
 
 
-n_train = 1000
+n_train = 10000
 X_train = X[:n_train]
 Y_train = Y[:n_train]
 X_vali = X[10001:12000]
@@ -30,11 +30,11 @@ X_test = X[12001:]
 Y_test = Y[12001:]
 
 # Create batches
-batch_size = 100 
+batch_size = 50
 
 # Initialize the Batch tensors
-X_train_batches = tn.zeros((int(n_train/batch_size),batch_size,27),dtype = tn.int64)
-Y_train_batches = tn.zeros((int(n_train/batch_size),batch_size,27),dtype = tn.int64)
+X_train_batches = th.zeros((int(n_train/batch_size),batch_size,27),dtype = th.int64)
+Y_train_batches = th.zeros((int(n_train/batch_size),batch_size,27),dtype = th.int64)
 
 # Create the batches
 for batch in range(int(n_train/batch_size)):
@@ -47,159 +47,36 @@ for batch in range(int(n_train/batch_size)):
 #src_mask_test = tn.zeros((int(n_train/batch_size),batch_size*8,27,27))
 #tgt_mask_test = tn.zeros((int(n_train/batch_size),batch_size*8,27,27))
 
-src_key_masks = tn.zeros((int(n_train/batch_size),batch_size,27),dtype = tn.bool)
-tgt_key_masks = tn.zeros((int(n_train/batch_size),batch_size,27),dtype=tn.bool)
+src_key_masks = th.ones((int(n_train/batch_size),batch_size,27,d_model))
+tgt_key_masks = th.ones((int(n_train/batch_size),batch_size,27,d_model))
 
 idx_sample = 0
 for batch in range(int(n_train/batch_size)):
     for sample in range(batch_size):
         
-        src_key_masks[batch,sample][-Paddings_fr[idx_sample]:] = 1
-        tgt_key_masks[batch,sample][-Paddings_en[idx_sample]:] = 1
-
-        # for i in range(27):
-        #     if np.random.rand() < 0.1:
-        #         tgt_key_masks[batch,sample][i] = True
-
-        #tgt_mask_test[batch,idx_sample*8:idx_sample*8+8] = tgt_mask(27,Paddings_en[idx_sample])
-        #src_mask_test[batch,idx_sample*8:idx_sample*8+8] = src_mask(27,Paddings_fr[idx_sample])
+        src_key_masks[batch,sample][-Paddings_fr[idx_sample]:] = 0
+        tgt_key_masks[batch,sample][-Paddings_en[idx_sample]:] = 0
 
         idx_sample += 1
 
+# Validation Masks
+src_key_masks_vali = th.ones(len(X_vali),27,d_model)
 
+for sample in range(len(X_vali)):
+    src_key_masks_vali[sample][-Paddings_fr[sample+n_train+1]:] = 0
 
-no_ahead_mask = tn.triu(tn.full((27,27),float('-inf')),diagonal=1)
-#%%
-import torch as th
-X = th.tensor([1,1,2,2,3])
-Y = th.tensor([1,1,2,2,3])
-Model = Transformer(10,10,d_model=10,nhead=5)
+# Test Masks
+src_key_masks_test = th.ones(len(X_test),27,d_model)
 
-#%%
-loss_fn = tn.nn.CrossEntropyLoss()
-Model = Transformer(src_vocab_size,tgt_vocab_size,d_model=128,nhead=4)
-optimizer = tn.optim.Adam(Model.parameters(), lr=0.01)
+for sample in range(len(X_test)):
+    src_key_masks_test[sample][-Paddings_fr[sample+n_train+1+len(X_vali)]:] = 0
 
+with open('data/Train_data.pkl', 'wb') as f:
+    pl.dump([X_train_batches,Y_train_batches,src_key_masks,tgt_key_masks],f)
 
+with open('data/Validation_data.pkl', 'wb') as f:
+    pl.dump([X_vali,Y_vali,src_key_masks_vali],f)
 
-out = Model(X_train_batches[0][2],Y_train_batches[0][1],tgt_mask = no_ahead_mask,src_key_padding_mask = src_key_masks[0][0],tgt_key_padding_mask = tgt_key_masks[0][0])
+with open('data/Test_data.pkl', 'wb') as f:
+    pl.dump([X_test,Y_test,src_key_masks_test],f)
 
-
-pred = out.argmax(1).tolist()
-Sentece = Vocab_en.lookup_tokens(pred)
-Sentece
-#%%
-
-def trainmodel(epochs):
-    if tn.cuda.is_available():
-        device = tn.device("cuda")
-    elif tn.backends.mps.is_available():
-        device = "mps"
-    else:
-        device = "cpu"
-
-    Model.to(device)
-    X_train_batches_mps = X_train_batches.to(device)
-    Y_train_batches_mps = Y_train_batches.to(device)
-    no_ahead_mask_mps = no_ahead_mask.to(device)
-
-
-    for epoch in tqdm(range(epochs)):
-        loss_epoch = 0
-
-        for X_batch,Y_batch,src_m,tgt_m in zip(X_train_batches_mps,Y_train_batches_mps,src_key_masks.to(device),tgt_key_masks.to(device)):
-
-            optimizer.zero_grad()
-            out = Model(X_batch,Y_batch,
-                        tgt_mask = no_ahead_mask_mps,
-                        src_key_padding_mask = src_m,
-                        tgt_key_padding_mask = tgt_m
-                        )
-
-            loss = 0
-            for i in range(batch_size):
-                #index = [Y_batch[i] != 205]
-                #loss += loss_fn(out[i][index],Y_batch[i][index])
-                loss += loss_fn(out[i],Y_batch[i])
-            #loss = loss_fn(out,Y_batch)
-            loss_epoch += loss.item()
-
-            loss.backward()
-            
-            optimizer.step()
-        print(f"Epoch: {epoch} Loss: {loss_epoch/n_train}")    
-
-#%%
-
-trainmodel(100)
-
-tn.save(Model.state_dict(), "Transformer.pt")
-        
-#%%
-Pred_mask = tn.tensor([False]+[True for i in range(26)],dtype=tn.bool)
-Model.to("cpu")
-out = Model(X_train_batches[0][0],Y_train_batches[0][0],tgt_mask = no_ahead_mask,src_key_padding_mask = src_key_masks[0][0],tgt_key_padding_mask = Pred_mask)
-pred = out.argmax(1).tolist()
-Sentece = Vocab_en.lookup_tokens(pred)
-print(Sentece)
-
-#%%
-
-src_mask_key = tn.zeros(27,dtype=tn.bool)
-src_mask_key[-Paddings_fr[-1]:] = 1
-
-
-Pred_mask = tn.tensor([True for i in range(27)],dtype=tn.bool)
-
-
-st_input = tn.tensor([206]+[205 for i in range(26)])
-
-
-
-
-for i in range(26):
-
-    Pred_mask[i] = False
-
-    out = Model(X_test[-1],
-                #Y_test[-1],
-                st_input,
-                src_mask = None,
-                tgt_mask = no_ahead_mask,
-
-                src_key_padding_mask = src_mask_key,
-                tgt_key_padding_mask = Pred_mask
-                )
-    pred = out.argmax(1).tolist()
-    st_input[i+1] = pred[i+1]
-
-Sentece = Vocab_en.lookup_tokens(st_input.tolist())
-print(Sentece)
-
-
-# #%%
-# import torch as tn
-
-# src_vocab_size = Vocab_fr.__len__()+1
-# tgt_vocab_size = Vocab_en.__len__()
-
-# model = Transformer(src_vocab_size,tgt_vocab_size,d_model=512 ,nhead=8)
-# model.load_state_dict(tn.load('Transformer.pt',map_location=tn.device('cpu')))
-# model.eval()
-
-# #%%
-
-# st_input = tn.tensor([206]+[205 for i in range(26)])
-
-# out = model(X_test[-8],st_input)
-# pred = out.argmax(1).tolist()
-# Sentece = Vocab_en.lookup_tokens(pred)
-# Sentece
-
-#%%
-# for X_batch,Y_batch in zip(X_train_batches,Y_train_batches):
-#             out = Model(X_batch,Y_batch)
-
-
-
-# %%
